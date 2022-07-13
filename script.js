b416f37d2f68d175458e7654f8bc3c9897437e21
tupudipi <tupudipi@gmail.com>
2022-07-13 10:39:33 +0300

    commit

--- script.js
+++ script.js
@@ -19,77 +19,118 @@ function playRound(playerSelection, computerSelection){
         if(computerSelection == "paper"){
             computerScore++;
             return `You picked ${playerSelection} - Computer picked ${computerSelection}.
-             You lose!
-             Current score:
-             You ${playerScore} - Computer ${computerScore}`;
+             You lose!`;
         }
         else{
             playerScore++;
             return `You picked ${playerSelection} - Computer picked ${computerSelection}.
-             You win!
-             Current score:
-              You ${playerScore} - Computer ${computerScore}`;
+             You win!`;
         }
     }
     else if(playerSelection == "paper"){
         if(computerSelection == "scissors"){
             computerScore++;
             return `You picked ${playerSelection} - Computer picked ${computerSelection}. 
-            You lose!
-            Current score:
-             You ${playerScore} - Computer ${computerScore}`;
+            You lose!`;
         }
         else{
             playerScore++;
             return `You picked ${playerSelection} - Computer picked ${computerSelection}. 
-            You win!
-            Current score: 
-            You ${playerScore} - Computer ${computerScore}`;
+            You win!`;
         }
     }
     else if(playerSelection == "scissors"){
         if(computerSelection == "rock"){
             computerScore++;
             return `You picked ${playerSelection} - Computer picked ${computerSelection}. 
-            You lose!
-            Current score: 
-            You ${playerScore} - Computer ${computerScore}`;
+            You lose!`;
         }
         else{
             playerScore++;
             return `You picked ${playerSelection} - Computer picked ${computerSelection}.
-            You win!
-            Current score: 
-            You ${playerScore} - Computer ${computerScore}`;
+            You win!`;
         }
     }
 }
 
-function game(){
-    let round = 1;
-    while(round <= 5){
-        const computerSelection = computerPlay();
-        const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
-        const result = playRound(playerSelection, computerSelection);
-        alert(result);
-        round++;
-    }
-    if(round > 5){
-        alert(`Game over!
-        Final score:
-        You ${playerScore} - Computer ${computerScore}`);
+// function game(){
+//     let round = 1;
+//     while(round <= 5){
+//         const computerSelection = computerPlay();
+//         const playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
+//         const result = playRound(playerSelection, computerSelection);
+//         alert(result);
+//         round++;
+//     }
+//     if(round > 5){
+//         alert(`Game over!
+//         Final score:
+//         You ${playerScore} - Computer ${computerScore}`);
         
-        const playAgain = prompt("Would you like to play again? (y/n)").toLowerCase();
-        if(playAgain == "y"){
-            playerScore = 0;
-            computerScore = 0;
-            round = 1;
-            game();
-        }
-    }
+//         const playAgain = prompt("Would you like to play again? (y/n)").toLowerCase();
+//         if(playAgain == "y"){
+//             playerScore = 0;
+//             computerScore = 0;
+//             round = 1;
+//             game();
+//         }
+//     }
+// }
+
+function initUI(){
+    const rock = document.createElement("button");
+    rock.innerHTML = "Rock";
+    rock.setAttribute("id", "rock");
+    rock.addEventListener("click", function(){
+        const computerSelection = computerPlay();
+        const result = playRound("rock", computerSelection);
+        updateScore();
+        roundResultText.innerHTML = result;
+    });
+    document.body.appendChild(rock);
+
+    const paper = document.createElement("button");
+    paper.innerHTML = "Paper";
+    paper.setAttribute("id", "paper");
+    paper.addEventListener("click", function(){
+        const computerSelection = computerPlay();
+        const result = playRound("paper", computerSelection);
+        updateScore();
+        roundResultText.innerHTML = result;
+    });
+    document.body.appendChild(paper);
+
+    const scissors = document.createElement("button");
+    scissors.innerHTML = "Scissors";
+    scissors.setAttribute("id", "scissors");
+    scissors.addEventListener("click", function(){
+        const computerSelection = computerPlay();
+        const result = playRound("scissors", computerSelection);
+        updateScore();
+        roundResultText.innerHTML = result;
+    });
+    document.body.appendChild(scissors);
+
+    const roundResultText = document.createElement("p");
+    roundResultText.innerHTML = "";
+    document.body.appendChild(roundResultText);
+
+    const score = document.createElement("p");
+    score.id = "score";
+    score.innerHTML = `Current score:
+    You ${playerScore} - Computer ${computerScore}`;
+    document.body.appendChild(score);
+}
+
+function updateScore(){
+    const score = document.querySelector("#score");
+    score.innerHTML = `Current score:
+    You ${playerScore} - Computer ${computerScore}`;
 }
 
 let playerScore = 0;
 let computerScore = 0;
-game();
+
+initUI();
+
 
